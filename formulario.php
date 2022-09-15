e48618a17b1a16390dfa8da40e53afe1280e5483
Deyvid Oliveira Silva <Deyvid4900@gmail.com>
2022-09-15 00:32:40 -0300

    Projeto-Android-01

---
+++ formulario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+<form action="formularioPHP.html" method="get">
+    echo "<input type='number' name's$c' <br><br>>"
+    </form>
+    
+
+
+    ?>
+    
+</body>
+</html>
